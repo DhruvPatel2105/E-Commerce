d2a39d356702f7c2fde290c580f777fc71de6d3c
LadolaVatsal17 <ladolavatsal17@gmail.com>
2023-10-11 01:32:39 -0400

    Login Funcation Done With Cookie and Auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,3 +22,8 @@ use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+});
