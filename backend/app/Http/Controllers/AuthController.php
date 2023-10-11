732193cd3a23413d1a39cbae63220ec56110cd55
LadolaVatsal17 <ladolavatsal17@gmail.com>
2023-10-11 00:40:50 -0400

    Validation Add to Registration Done.

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
         $user = User::create([
             'first_name' => $request->input('first_name'),
