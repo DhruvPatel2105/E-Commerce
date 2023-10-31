a5a52a0dd94a9c85e3c8b240e07f334ff3c9eac1
LadolaVatsal17 <ladolavatsal17@gmail.com>
2023-10-31 11:13:03 -0400

    user crud operation done.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +28,14 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+    // Route::get('users',[UserController::class,'index']);
+    // Route::post('users',[UserController::class,'store']);
+    // Route::get('users/{id}',[UserController::class,'show']);
+    // Route::put('users/{id}',[UserController::class,'update']);
+    // Route::delete('users/{id}',[UserController::class,'destroy']);
+
+    Route::apiResource('users',UserController::class);
+
+
 });
