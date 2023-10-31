851df353144186d04c3a054a8465a8e8c36d1fd4
Dhruv Patel <dhruvpatel10890@gmail.com>
2023-10-31 09:55:47 -0400

    updated code..

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicModule } from './public/public.module';
 import { SecureModule } from './secure/secure.module';
-import { HttpClientModule} from '@angular/common/http';
-
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CredentialInterceptor } from './interceptors/credential.interceptor';
 
 @NgModule({
   declarations: [
@@ -20,7 +18,13 @@ import { HttpClientModule} from '@angular/common/http';
     PublicModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CredentialInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
