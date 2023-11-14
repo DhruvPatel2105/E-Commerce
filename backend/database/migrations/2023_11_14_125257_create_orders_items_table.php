1e15cf3052010600cd8badf928b1a73ceac850b3
LadolaVatsal17 <ladolavatsal17@gmail.com>
2023-11-14 08:01:49 -0500

    Implement Order Feature In Backend Done

---
+++ backend/database/migrations/2023_11_14_125257_create_orders_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->string('product_title');
+            $table->decimal('price');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders_items');
+    }
+};
