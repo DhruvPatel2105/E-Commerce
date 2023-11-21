1554f05200f109b54d01bd9d4265fbab2186c7b0
Dhruv Patel <dhruvpatel10890@gmail.com>
2023-11-21 15:21:05 -0500

    updated code with the orders component

--- frontend/src/app/services/orders.service.ts
+++ frontend/src/app/services/orders.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {RestService} from './rest.service';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class OrdersService {
+export class OrderService extends RestService {
+  endpoint = `${environment.api}/orders`;
 
-  constructor() { }
+  export(): Observable<any> {
+    return this.http.post(`${environment.api}/export`, {}, {responseType: 'blob'});
+  }
+
+  chart(): Observable<any> {
+    return this.http.get(`${environment.api}/chart`);
+  }
 }
