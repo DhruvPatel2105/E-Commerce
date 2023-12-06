1033d47f5a6c20083142164337569dcbb25abb94
LadolaVatsal17 <ladolavatsal17@gmail.com>
2023-12-06 02:14:52 -0500

    order export in csv file done

--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -21,4 +21,36 @@ class OrderController extends Controller
 
         return new OrderResource($order);
     }
+
+
+    public function export()
+    {
+        $headers = [
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=orders.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0",
+        ];
+
+        $callback = function () {
+            $orders = Order::all();
+            $file = fopen('php://output', 'w');
+
+            fputcsv($file, ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']);
+
+            foreach ($orders as $order) {
+                fputcsv($file, [$order->id, $order->name, $order->email, '', '', '']);
+
+                foreach ($order->orderItems as $orderItem) {
+                    fputcsv($file, ['', '', '', $orderItem->product_title, $orderItem->price, $orderItem->quantity]);
+                }
+            }
+
+            fclose($file);
+        };
+
+        return \Response::stream($callback, 200, $headers);
+    }
+
 }
